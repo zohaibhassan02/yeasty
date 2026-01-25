5573f1588f82103c6c0be86ea6ecd24eae805591
zohaibhassan02 <98820921+zohaibhassan02@users.noreply.github.com>
2026-01-25 13:51:16 -0400

    Update route.ts

--- app/api/user/userSignUp/route.ts
+++ app/api/user/userSignUp/route.ts
@@ -3,48 +3,53 @@ import { NextResponse, NextRequest } from 'next/server';
 import UserAuthModal from "@/backend/Model/UserAuthModal";
 import bcrypt from 'bcryptjs';
 
-export async function POST(request: NextRequest) {
-    try {
-        await connect();
+// export async function POST(request: NextRequest) {
+//     try {
+//         await connect();
 
-        const { fullName, email, password, companyName, hearFrom  } = await request.json();
+//         const { fullName, email, password, companyName, hearFrom  } = await request.json();
   
-        const user = await UserAuthModal.findOne({ email: email }).exec();
-        if (user) {
-          return NextResponse.json({
-            status: "error",
-            message: "User already exists",
-          }, {
-            status: 400,
-          });
-        }
+//         const user = await UserAuthModal.findOne({ email: email }).exec();
+//         if (user) {
+//           return NextResponse.json({
+//             status: "error",
+//             message: "User already exists",
+//           }, {
+//             status: 400,
+//           });
+//         }
 
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
+//         const salt = await bcrypt.genSalt(10);
+//         const hashedPassword = await bcrypt.hash(password, salt);
     
-        const newUser = new UserAuthModal({
-          fullName,
-          email,
-          password: hashedPassword,
-          companyName,
-          hearFrom,
-        });
+//         const newUser = new UserAuthModal({
+//           fullName,
+//           email,
+//           password: hashedPassword,
+//           companyName,
+//           hearFrom,
+//         });
     
-        await newUser.save();
-        return NextResponse.json({
-          status: "success",
-          message: "User account created successfully",
-        },{
-            status: 200,
-        });
+//         await newUser.save();
+//         return NextResponse.json({
+//           status: "success",
+//           message: "User account created successfully",
+//         },{
+//             status: 200,
+//         });
   
-    }
-    catch (error) {
-        console.log(error);
-        return NextResponse.json({
-          message: "Something went wrong",
-        },{
-          status: 500,
-        });
-    }
+//     }
+//     catch (error) {
+//         console.log(error);
+//         return NextResponse.json({
+//           message: "Something went wrong",
+//         },{
+//           status: 500,
+//         });
+//     }
+// }
+
+export async function GET() {
+  return NextResponse.json({ ok: true, route: "app/api/user/userSignUp" });
 }
+
