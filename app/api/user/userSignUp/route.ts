6e12dd86ba55feb48309715a97ad7dfe97eaefb2
zohaibhassan02 <98820921+zohaibhassan02@users.noreply.github.com>
2026-01-25 14:38:07 -0400

    Update route.ts

--- app/api/user/userSignUp/route.ts
+++ app/api/user/userSignUp/route.ts
@@ -1,31 +1,50 @@
-export const runtime = "nodejs";
-export const dynamic = "force-dynamic";
+import { connect } from '@/backend/database/dbConfig';
+import { NextResponse, NextRequest } from 'next/server';
+import UserAuthModal from "@/backend/Model/UserAuthModal";
+import bcrypt from 'bcryptjs';
 
-import { NextResponse } from "next/server";
+export async function POST(request: NextRequest) {
+    try {
+        await connect();
 
-/**
- * This route does NOTHING except prove:
- * - POST works
- * - You are hitting the correct deployment
- */
+        const { fullName, email, password, companyName, hearFrom  } = await request.json();
+  
+        const user = await UserAuthModal.findOne({ email: email }).exec();
+        if (user) {
+          return NextResponse.json({
+            status: "error",
+            message: "User already exists",
+          }, {
+            status: 400,
+          });
+        }
 
-export async function GET() {
-  return NextResponse.json({
-    ok: true,
-    method: "GET",
-    stamp: "USER_SIGNUP_ROUTE_GET_vFINAL",
-  });
-}
-
-export async function POST() {
-  return NextResponse.json({
-    ok: true,
-    method: "POST",
-    stamp: "USER_SIGNUP_ROUTE_POST_vFINAL",
-    time: new Date().toISOString(),
-  });
-}
-
-export async function OPTIONS() {
-  return new NextResponse(null, { status: 204 });
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+    
+        const newUser = new UserAuthModal({
+          fullName,
+          email,
+          password: hashedPassword,
+          companyName,
+          hearFrom,
+        });
+    
+        await newUser.save();
+        return NextResponse.json({
+          status: "success",
+          message: "User account created successfully",
+        },{
+            status: 200,
+        });
+  
+    }
+    catch (error) {
+        console.log(error);
+        return NextResponse.json({
+          message: "Something went wrong",
+        },{
+          status: 500,
+        });
+    }
 }
